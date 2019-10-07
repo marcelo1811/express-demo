c68ef05193cbd5de684f1bb446ed738dbe36d939
Marcelo <marcelo.1811@gmai.com>
2019-10-06 21:42:58 -0300

    feat: add middleware in separeted file

--- index.js
+++ index.js
@@ -86,5 +86,10 @@ const validateCourse = (course) => {
 }
 
 // PORT
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
+const port = 3000
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = {
+  app
+};
